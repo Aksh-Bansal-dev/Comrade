2239b04fd11bdbb1ef8698368c662711f49e967d
Aksh Bansal <akshmark@gmail.com>
2021-04-30 16:17:28 +0530

    added jest tests

---
+++ mae/src/utils/__tests__/getUrl.test.ts
@@ -0,0 +1,42 @@
+import getUrl from "../getUrl";
+
+describe("Making Link", () => {
+  test("Only tag", () => {
+    const queryString = "#event";
+    const pageNum = 0;
+
+    expect(getUrl(pageNum, queryString)).toBe("?_limit=10&_start=0&tags=event");
+  });
+  test("Only author", () => {
+    const queryString = "@user";
+    const pageNum = 0;
+
+    expect(getUrl(pageNum, queryString)).toBe(
+      "?_limit=10&_start=0&authorName=user"
+    );
+  });
+  test("2nd page", () => {
+    const queryString = "";
+    const pageNum = 1;
+
+    expect(getUrl(pageNum, queryString)).toBe("?_limit=10&_start=10");
+  });
+  test("Query with space", () => {
+    const queryString = " #event";
+    const pageNum = 0;
+
+    expect(getUrl(pageNum, queryString)).toBe("?_limit=10&_start=0");
+  });
+  test("Query with both tag and author", () => {
+    const queryString = "#event@user";
+    const pageNum = 0;
+
+    expect(getUrl(pageNum, queryString)).toBe("?_limit=10&_start=0");
+  });
+  test("Wrong tag", () => {
+    const queryString = "#events";
+    const pageNum = 0;
+
+    expect(getUrl(pageNum, queryString)).toBe("?_limit=10&_start=0");
+  });
+});
--- mae/src/utils/getUrl.ts
+++ mae/src/utils/getUrl.ts
@@ -1,3 +1,4 @@
+import { map } from "./tags";
 export const invalidRegex = /(#.+@)|(@.+#)|(\s)|[\!\"\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\>\=\?\[\]\{\}\\\\\^\_\`\~]/;
 
 type getUrlType = (pageNum: number, query: string) => string;
@@ -8,8 +9,13 @@ const getUrl: getUrlType = (pageNum, query) => {
     return base;
   } else {
     if (query[0] === "#") {
-      // return `/${query.substring(1)}${base}`;
-      return `${base}&tags=${query.substring(1)}`;
+      const tagName = query.substring(1);
+      // @ts-ignore
+      if (map.has(tagName)) {
+        return `${base}&tags=${tagName}`;
+      } else {
+        return base;
+      }
     } else if (query[0] === "@") {
       return `${base}&authorName=${query.substring(1)}`;
     } else {
